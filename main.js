95bf4016d77d6a0745e9c71e3d2fa1f1ccfd2ce6
Melanie Hallowell <melleehall@gmail.com>
2019-12-06 16:55:17 -0700

    Second iteration of Dog API App

--- main.js
+++ main.js
@@ -2,25 +2,38 @@
 
 // call the Dog API to generate URLs to random dog images and then have them displayed in the console by calling displayResults
 function getDogImage(selectedNum) {
-    fetch(`https://dog.ceo/api/breeds/image/random/${selectedNum}`)
+  fetch(`https://dog.ceo/api/breeds/image/random/${selectedNum}`)
     // converts JSON back into in-memory JS object
     .then(response => response.json())
-    .then(responseJson => displayResults(responseJson))
+    // pass the JS object as an argument to the displayResults function
+    .then(responseJson => 
+      displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-// print the vale of the message key (dog image URLs) to the console
+// print the value of the message key (dog image URLs) to the console
 function displayResults(responseJson) {
-  console.log(responseJson.message);
+  const photoCount = responseJson.message.length
+  let newImages = [];
+  for (let i = 0; i < photoCount; i++) {
+    let newImage = `<img src="${responseJson.message[i]}" alt="dog image ${i}" class="results-img">`
+    newImages.push(newImage);
+  };
+    console.log(newImages)
+    $('.results-img').remove();
+    $('.dog-header').after(newImages);
+    // display the results section
+    $('.results').removeClass('hidden');
 }
 
-// when a number is submitted by user, pass it as an argument to the getDogImage function
+// when a number of requested photos is submitted by user, pass it as an argument to the getDogImage function
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     // store the number entered
     const num = $('.input').val();
     getDogImage(num);
+    console.log(num);
   });
 }
 
