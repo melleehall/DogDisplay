d7edc4004fc4e5813f57f8c35a66890c205601b1
Melanie Hallowell <melleehall@gmail.com>
2019-12-06 18:26:18 -0700

    Dog API App V3

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // call the Dog API to generate URLs to random dog images and then have them displayed in the console by calling displayResults
-function getDogImage(selectedNum) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${selectedNum}`)
+function getDogImage(selectedBreed) {
+  fetch(`https://dog.ceo/api/breed/${selectedBreed}/images`)
     // converts JSON back into in-memory JS object
     .then(response => response.json())
     // pass the JS object as an argument to the displayResults function
@@ -11,29 +11,34 @@ function getDogImage(selectedNum) {
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
+
 // print the value of the message key (dog image URLs) to the console
 function displayResults(responseJson) {
-  const photoCount = responseJson.message.length
-  let newImages = [];
-  for (let i = 0; i < photoCount; i++) {
+  if (responseJson.message == "Breed not found (master breed does not exist)") {
+    alert("Breed requested not available in Dog API.");
+  } else {
+    const breedPhotos = responseJson.message.length;
+    let newImages = [];
+    for (let i = 0; i < breedPhotos; i++) {
     let newImage = `<img src="${responseJson.message[i]}" alt="dog image ${i}" class="results-img">`
     newImages.push(newImage);
   };
-    console.log(newImages)
     $('.results-img').remove();
     $('.dog-header').after(newImages);
     // display the results section
     $('.results').removeClass('hidden');
+  };
 }
 
-// when a number of requested photos is submitted by user, pass it as an argument to the getDogImage function
+// when a breed is submitted by user, pass it as an argument to the checkForBreed function 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
     // store the number entered
-    const num = $('.input').val();
-    getDogImage(num);
-    console.log(num);
+    const breed = $('.input').val();
+    getDogImage(breed);
+    console.log(breed);
   });
 }
 
